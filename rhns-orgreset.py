f4562919f45cb0926c5c67e669a6445903a1dfb2
Felix Dewaleyne <fdewaley@redhat.com>
2014-01-14 12:16:54 +0000

    forgot to change the version with the previous changes

--- rhns-orgreset.py
+++ rhns-orgreset.py
@@ -4,7 +4,7 @@
 __author__ = "Felix Dewaleyne"
 __credits__ = ["Felix Dewaleyne"]
 __license__ = "GPL"
-__version__ = "2.0"
+__version__ = "2.2"
 __maintainer__ = "Felix Dewaleyne"
 __email__ = "fdewaley@redhat.com"
 __status__ = "stable"
