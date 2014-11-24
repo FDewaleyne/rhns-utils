71c50d54b16461ca88bb70ad6cfa55221fab6ef7
Felix Dewaleyne <fdewaley@redhat.com>
2014-11-24 16:25:47 +0000

    version update

--- db-tools/rhns-remove-package.py
+++ db-tools/rhns-remove-package.py
@@ -14,7 +14,7 @@
 __author__ = "Felix Dewaleyne"
 __credits__ = ["Felix Dewaleyne"]
 __license__ = "GPL"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __maintainer__ = "Felix Dewaleyne"
 __email__ = "fdewaley@redhat.com"
 __status__ = "beta"
