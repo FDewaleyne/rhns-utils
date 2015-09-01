76d1997ba9ab862486d8f0ccb8ca14c5e3c3da26
Felix Dewaleyne <fdewaley@redhat.com>
2015-09-01 15:21:20 +0100

    did changes required to the script - enough for v0.1

--- rhns-export-to-csv-per-sysgroup.py
+++ rhns-export-to-csv-per-sysgroup.py
@@ -14,10 +14,10 @@ __license__ = "GPL"
 __version__ = "0.1"
 __maintainer__ = "Felix Dewaleyne"
 __email__ = "fdewaley@redhat.com"
-__status__ = "prod"
+__status__ = "dev"
 
 
-import xmlrpclib, sys, getpass
+import xmlrpclib, sys, getpass, csv
 #connection part
 sys.stderr.write("Please input the fqdn of your satellite or its ip adress: ")
 sathost = raw_input().strip()
@@ -26,11 +26,13 @@ sys.stderr.write("enter your RHN login: ")
 login = raw_input().strip()
 password = getpass.getpass(prompt="enter your RHN Password: ")
 sys.stderr.write("\n")
-key = client.auth.login(login,password)
+key = client.auth.login(login, password)
 #remove the password from memory
 del password
 
-
+#gather the list of system groups
+for a_group in client.systemgroup.listAllGroups(key):
+    extract_sysgroup(a_group['name'])
 
 def extract_sysgroup(sysgroup):
     """generates a csv file for the systems in the sysgroup"""
@@ -38,20 +40,25 @@ def extract_sysgroup(sysgroup):
     print "Fetching the list of systems and the details for each of your system. This may take some time"
     rhn_data={}
     #change the call in this part
-    for system in client.system.listUserSystems(key):
+    for system in client.systemgroup.listSystems(key, sysgroup):
         #gather the details for each system
-        rhn_data[system['id']] = {'id': system['id'], 'last_checkin': system['last_checkin']}
+        rhn_data[system['id']] = {'id': system['id'], 'osa_status': system['osa_status'], 'last_boot': system['last_boot']}
         #add all the data from getDetails to this dictionary
         rhn_data[system['id']].update(client.system.getDetails(key,int(system['id'])))
         #add all the network details as well
         rhn_data[system['id']].update(client.system.getNetwork(key,int(system['id'])))
+        #get the count of erratas available & unscheduled
+        try:
+            relevant_errata_count = len(client.system.getRelevantErrata(key, system['id']))
+            unscheduled_errata_count = len(client.system.getUnscheduledErrata(key, system['id']))
+            rhn_data[system['id']].update(['relevant_erratas': str(relevant_errata_count), 'unscheduled_erratas': str(unscheduled_errata_count) ])
+        except:
+            rhn_data[system['id']].update(['relevant_erratas': 'NA', 'unscheduled_erratas': 'NA' ])
 
-    client.auth.logout(key)
     #now write the csv file
     print "Writing data to the csv file"
-    headers=['id' , 'profile_name', 'hostname', 'ip', 'last_checkin', 'base_entitlement', 'description', 'adress1','adress2', 'city', 'state', 'country', 'building', 'room', 'rack']
-    import csv
-    csvfile = open('systems_'+login+'.csv', 'wb' )
+    headers=['id' , 'profile_name', 'hostname', 'ip', 'osa_status', 'last_boot', 'base_entitlement', 'relevant_erratas', 'unscheduled_erratas', 'description', 'adress1','adress2', 'city', 'state', 'country', 'building', 'room', 'rack']
+    csvfile = open('systems_'+sysgroup+'.csv', 'wb' )
     csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
     csv_writer.writerow(headers)
     for system in rhn_data.itervalues():
@@ -61,3 +68,6 @@ def extract_sysgroup(sysgroup):
         csv_writer.writerow(line)
         del line
     del rhn_data
+
+#end of the script
+client.auth.logout(key)
