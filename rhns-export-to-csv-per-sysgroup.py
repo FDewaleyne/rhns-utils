d86092c1315334d7d9dc694f74b3e0209fe9ff9d
Felix Dewaleyne <fdewaley@redhat.com>
2015-09-01 14:50:27 +0100

    changed connection parameters

--- rhns-export-to-csv-per-sysgroup.py
+++ rhns-export-to-csv-per-sysgroup.py
@@ -11,7 +11,7 @@
 __author__ = "Felix Dewaleyne"
 __credits__ = ["Felix Dewaleyne"]
 __license__ = "GPL"
-__version__ = "0.4"
+__version__ = "0.1"
 __maintainer__ = "Felix Dewaleyne"
 __email__ = "fdewaley@redhat.com"
 __status__ = "prod"
@@ -19,7 +19,9 @@ __status__ = "prod"
 
 import xmlrpclib, sys, getpass
 #connection part
-client = xmlrpclib.Server("https://xmlrpc.rhn.redhat.com/rpc/api")
+sys.stderr.write("Please input the fqdn of your satellite or its ip adress: ")
+sathost = raw_input().strip()
+client = xmlrpclib.Server("https://%s/rpc/api" % (sathost))
 sys.stderr.write("enter your RHN login: ")
 login = raw_input().strip()
 password = getpass.getpass(prompt="enter your RHN Password: ")
@@ -28,29 +30,34 @@ key = client.auth.login(login,password)
 #remove the password from memory
 del password
 
-#fetch the data
-print "Fetching the list of systems and the details for each of your system. This may take some time"
-rhn_data={}
-for system in client.system.listUserSystems(key):
-    #gather the details for each system
-    rhn_data[system['id']] = {'id': system['id'], 'last_checkin': system['last_checkin']}
-    #add all the data from getDetails to this dictionary
-    rhn_data[system['id']].update(client.system.getDetails(key,int(system['id'])))
-    #add all the network details as well
-    rhn_data[system['id']].update(client.system.getNetwork(key,int(system['id'])))
 
-client.auth.logout(key)
-#now write the csv file
-print "Writing data to the csv file"
-headers=['id' , 'profile_name', 'hostname', 'ip', 'last_checkin', 'base_entitlement', 'description', 'adress1','adress2', 'city', 'state', 'country', 'building', 'room', 'rack']
-import csv
-csvfile = open('systems_'+login+'.csv', 'wb' )
-csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-csv_writer.writerow(headers)
-for system in rhn_data.itervalues():
-    line = []
-    for value in headers:
-        line.append(system.get(value))
-    csv_writer.writerow(line)
-    del line
-del rhn_data
+
+def extract_sysgroup(sysgroup):
+    """generates a csv file for the systems in the sysgroup"""
+    #fetch the data
+    print "Fetching the list of systems and the details for each of your system. This may take some time"
+    rhn_data={}
+    #change the call in this part
+    for system in client.system.listUserSystems(key):
+        #gather the details for each system
+        rhn_data[system['id']] = {'id': system['id'], 'last_checkin': system['last_checkin']}
+        #add all the data from getDetails to this dictionary
+        rhn_data[system['id']].update(client.system.getDetails(key,int(system['id'])))
+        #add all the network details as well
+        rhn_data[system['id']].update(client.system.getNetwork(key,int(system['id'])))
+
+    client.auth.logout(key)
+    #now write the csv file
+    print "Writing data to the csv file"
+    headers=['id' , 'profile_name', 'hostname', 'ip', 'last_checkin', 'base_entitlement', 'description', 'adress1','adress2', 'city', 'state', 'country', 'building', 'room', 'rack']
+    import csv
+    csvfile = open('systems_'+login+'.csv', 'wb' )
+    csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+    csv_writer.writerow(headers)
+    for system in rhn_data.itervalues():
+        line = []
+        for value in headers:
+            line.append(system.get(value))
+        csv_writer.writerow(line)
+        del line
+    del rhn_data
